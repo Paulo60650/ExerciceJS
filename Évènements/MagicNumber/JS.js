d84d6533dc8a5133cf2f8ab2577e075bbdc18bcd
Paulo60650 <58972637+Paulo60650@users.noreply.github.com>
2020-01-13 16:17:08 +0100

    optimisation magic

--- Évènements/MagicNumber/JS.js
+++ Évènements/MagicNumber/JS.js
@@ -1,48 +1,21 @@
 var magic = parseInt(Math.random()*100);
-var element = document.getElementById('Roger');
-element.addEventListener("keypress", function control(event){
-    var nb = document.getElementById("Roger");
-    var res = nb.value;
-    if (event.which === 13 && res > magic)
-    {
-        document.getElementById('test').textContent = "Trop grand ! Réessayez";
-        document.getElementById('Roger').value = "";
-        event.preventDefault();
-    } 
-    else if( (event.which === 13 && res < magic))
-    {
-        document.getElementById('test').textContent = "Trop petit ! Réessayez";
-        document.getElementById('Roger').value = "";
-        event.preventDefault();
-    }
-    else if(res == magic)
-    {
-        alert("Félitations vous avez gagné");
-        if(confirm("Voulez vous recommencer ?") == true)
-        {
-            window.location.reload();
-        }
-        else 
-        {
-            window.close();
-        }
-    }   
-});
-var element = document.getElementById('button');
-element.addEventListener("click", function control(event){
-    var nb = document.getElementById("Roger");
-    var res = nb.value;
+var element_input = document.getElementById('Roger');
+
+
+
+function func(){
+
+var nb = document.getElementById("Roger");
+var res = nb.value;
     if (res > magic)
     {
         document.getElementById('test').textContent = "Trop grand ! Réessayez";
         document.getElementById('Roger').value = "";
-        event.preventDefault();
     } 
     else if( res < magic)
     {
         document.getElementById('test').textContent = "Trop petit ! Réessayez";
         document.getElementById('Roger').value = "";
-        event.preventDefault();
     }
     else if(res == magic)
     {
@@ -57,4 +30,12 @@ element.addEventListener("click", function control(event){
         }
         
     }
+}
+element_input.addEventListener("keypress", function control(event){
+    if (event.which === 13)
+        func()  
+});
+var element_button = document.getElementById('button');
+element_button.addEventListener("click", function(){
+    func();
 });
